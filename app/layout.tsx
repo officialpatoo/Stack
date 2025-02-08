79587c9da22baefe1411aeecb2b3c75f6d572d67
PATOOWORLD <support@patooworld.com>
2025-02-08 04:58:10 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Provider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Stack Template",
+  title: "PATOOWORLD SPACE",
   description: "A Multi-tenant Next.js Starter Template",
 };
 
