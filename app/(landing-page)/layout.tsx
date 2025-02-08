79f013a2acea107597aff87a6a021d4d8c870b02
PATOOWORLD <support@patooworld.com>
2025-02-08 04:29:30 -0800

    Update layout.tsx

--- app/(landing-page)/layout.tsx
+++ app/(landing-page)/layout.tsx
@@ -6,19 +6,19 @@ export default function Layout(props: { children: React.ReactNode }) {
     <div className="flex min-h-screen flex-col">
       <LandingPageHeader
         items={[
-          { title: "Home", href: "/" },
+          { title: "Home", href: "https://genai.patooworld.com/" },
           { title: "Features", href: "/#features" },
           { title: "Pricing", href: "/#pricing" },
-          { title: "Github", href: "https://github.com/stack-auth/stack-template", external: true },
+          { title: "Github", href: "https://github.com/patooworld", external: true },
         ]}
       />
       <main className="flex-1">{props.children}</main>
       <Footer
-        builtBy="Stack Auth"
-        builtByLink="https://stack-auth.com/"
-        githubLink="https://github.com/stack-auth/stack-template"
-        twitterLink="https://twitter.com/stack_auth"
-        linkedinLink="linkedin.com/company/stack-auth"
+        builtBy="Patooworld Auth"
+        builtByLink="https://www.patooworld.com/"
+        githubLink="https://github.com/officialpatoo/stack"
+        twitterLink="https://twitter.com/patooworld"
+        linkedinLink="linkedin.com/company/patooworld"
       />
     </div>
   );
