6ef178ebfaeb9fba5f1383443da366b261bbf654
PATOOWORLD <support@patooworld.com>
2025-04-23 09:47:22 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { PageClient } from "./page-client";
 
 export const metadata = {
-  title: "Dashboard - Stack Template",
+  title: "Dashboard - PATOOWORLD",
 };
 
 export default function Dashboard() {
